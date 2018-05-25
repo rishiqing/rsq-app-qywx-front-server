a78948fa2e99c44ae95cb297c429340a4b6ae202
Wallace Mao <maowenqiang0752@163.com>
2018-05-25 15:45:35 +0800

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const os = require('os')
+const path = require('path')
+const fs = require('fs')
+const express = require('express')
+const app = express()
+
+const homePath = os.homedir()
+const placeholder = '{{rsqConfig}}'
+const configPath = path.resolve(homePath, 'qywx', 'qywx-front-server.json')
+const templatePath = path.resolve(__dirname, 'view', 'index-server.template.html')
+const indexPath = path.resolve(__dirname, 'view', 'index.html')
+
+const config = fs.readFileSync(configPath, 'UTF-8')
+
+const template = fs.readFileSync(templatePath, 'UTF-8')
+
+const result = template.replace(placeholder, config)
+fs.writeFileSync(indexPath, result, 'UTF-8')
+console.log('result: ' + result)
+
+app.get('/', (req, res) => {
+    res.end('ok')
+})
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(indexPath)
+})
+
+app.listen(3000, () => {
+    console.log('qywx front app listening on port 3000!')
+})
