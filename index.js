44b1e3ea90ebc475f4d9f60ee5cd4eb28dc388ec
Wallace Mao <maowenqiang0752@163.com>
2018-05-28 12:36:04 +0800

    add version

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const os = require('os')
-const path = require('path')
-const fs = require('fs')
-const express = require('express')
-const app = express()
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const homePath = os.homedir()
-const placeholder = '{{rsqConfig}}'
-const configPath = path.resolve(homePath, 'qywx', 'qywx-front-server.json')
-const templatePath = path.resolve(__dirname, 'view', 'index.template.html')
-const indexPath = path.resolve(__dirname, 'view', 'index.html')
+const homePath = os.homedir();
+const placeholder = '{{rsqConfig}}';
+const configPath = path.resolve(homePath, 'qywx', 'qywx-front-server.json');
+const templatePath = path.resolve(__dirname, 'view', 'index.template.html');
+const indexPath = path.resolve(__dirname, 'view', 'index.html');
 
-const config = fs.readFileSync(configPath, 'UTF-8')
+const config = fs.readFileSync(configPath, 'UTF-8');
 
-const template = fs.readFileSync(templatePath, 'UTF-8')
+const template = fs.readFileSync(templatePath, 'UTF-8');
 
-const result = template.replace(placeholder, config)
-fs.writeFileSync(indexPath, result, 'UTF-8')
+const result = template.replace(placeholder, config);
+fs.writeFileSync(indexPath, result, 'UTF-8');
 
 app.get('/', (req, res) => {
-    res.end('ok')
-})
+    res.end('ok');
+});
 
 app.get('/qywxbackwebapp/index.html', (req, res) => {
-    res.sendFile(indexPath)
-})
+    res.sendFile(indexPath);
+});
 
 app.listen(3000, () => {
-    console.log('qywx front app listening on port 3000!')
-})
+    console.log('qywx front app listening on port 3000!');
+});
